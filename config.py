070cfe7e9041a7912033e7959062c1f3736f73df
Fredrik.Sundstrom <fsund001@gmail.com>
2020-02-22 12:46:26 +0000

    added missing commas in list and rm .idea dir

--- config.py
+++ config.py
@@ -7,13 +7,13 @@ CLIENT_ID = 'ENTER_YOUR_CLIENT_ID'
 CLIENT_SECRET = 'ENTER_YOUR_SECRET_ID'
 USER_ID = 'ENTER_YOUR_SPOTIFY_USERNAME'
 REDIRECT_URI = 'http://localhost:8888/callback'
-SCOPES = ['user-library-read'
-          'playlist-read-private'
-          'user-read-private'
-          'user-read-email'
-          'user-read-birthdate'
-          'playlist-modify-public'
-          'playlist-modify-private'
+SCOPES = ['user-library-read',
+          'playlist-read-private',
+          'user-read-private',
+          'user-read-email',
+          'user-read-birthdate',
+          'playlist-modify-public',
+          'playlist-modify-private',
           ]
 
 if 'ENTER_YOUR' in CLIENT_ID or 'ENTER_YOUR' in CLIENT_SECRET or 'ENTER_YOUR' in USER_ID:
