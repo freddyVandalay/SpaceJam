55cd28a01a5c68987210ef2ce86481f57c899fe2
fredrik.sundstrom <freddyvandalay@gmail.com>
2024-09-22 16:22:42 +0100

    Add black and run fmt on all files
    fmt

--- src/login.py
+++ src/login.py
@@ -4,16 +4,22 @@ import spotipy.util as util
 import config
 
 
-client_credentials_manager = SpotifyClientCredentials(client_id=config.CLIENT_ID, client_secret=config.CLIENT_SECRET)
+client_credentials_manager = SpotifyClientCredentials(
+    client_id=config.CLIENT_ID, client_secret=config.CLIENT_SECRET
+)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-token = util.prompt_for_user_token(config.USER_ID, config.SCOPES,
-                                   client_id=config.CLIENT_ID,
-                                   client_secret=config.CLIENT_SECRET,
-                                   redirect_uri=config.REDIRECT_URI
-                                   )
+token = util.prompt_for_user_token(
+    config.USER_ID,
+    config.SCOPES,
+    client_id=config.CLIENT_ID,
+    client_secret=config.CLIENT_SECRET,
+    redirect_uri=config.REDIRECT_URI,
+)
 if token:
     sp = spotipy.Spotify(auth=token)
 else:
     print("Can't get token for {}".format(config.USER_ID))
 
-def getToken(): return token
+
+def getToken():
+    return token
--- tests/unitTest.py
+++ tests/unitTest.py
@@ -1,3 +1,2 @@
 class unitTest:
-	
-		
+    pass
