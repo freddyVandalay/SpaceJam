f9105c86801d9f26a9bfbe1f2b1e0db30eb63811
fredrik.sundstrom <freddyvandalay@gmail.com>
2024-09-22 19:02:20 +0100

    Add spotify client

--- src/spotify_api.py
+++ src/spotify_api.py
@@ -1,2 +1,12 @@
-class SpotifyClient:
-    pass
+from spotipy.oauth2 import SpotifyClientCredentials
+
+class SpotifyClient(SpotifyClientCredentials):
+    def __init__(self, client_id=None, client_secret=None, proxies=None):
+        super().__init__(client_id, client_secret, proxies)
+
+    def _get_client(self):
+        pass
+    
+    def _generate_token(self):
+        pass
+    
